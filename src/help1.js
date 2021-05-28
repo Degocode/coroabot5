658caac47147317de31ba2fc39072955f36b957d
Degocode <80612531+Degocode@users.noreply.github.com>
2021-05-28 18:57:37 -0300

    Update help1.js

--- src/help1.js
+++ src/help1.js
@@ -1,14 +1,53 @@
 const help1 = (prefix) => {
+return `
+*┌────MENUADM─────*
 
-	return `
-┣━━━━°❀ ❬ *Logo vamos pensar o que fazer aqui* ❭ ❀°━━━━┓
-┃
-┣⊱❥ *xd*
-┣⊱❥ *pinterest*
-════════════════════
-*André*
-*Digite dono para mais info*
-════════════════════`
+*┌─LEVEL NO GRUPO─*
+*│-.leveling [on/off]*
+*└────────────────*
+
+*┌────BEM VINDO───*
+*│-.bemvindo [1/0]*
+*└────────────────*
+
+*┌──PARA OS ADMS──*
+*│-.marcar*
+*│-.marcar2*
+*│-.marcar3*
+*│-.banir*
+*│-.add [@]*
+*│-.banir [@]*
+*│-.promover [@]*
+*│-.rebaixar*
+*│-.admins*
+*│-.linkgp*
+*│-.nomegp*
+*│-.bloqueados*
+*│-.bloquear [@]*
+*│-.desbloquear [@]*
+*│-.limpar*
+*│-.clonar [@]*
+*│-.desligar[so para o dono]*
+*│-.setfoto[alterar foto do grupo]*
+*│-.setnome[alterar nome do grupo]*
+*│-.setdesc[Mudar a descrição do grupo]*
+*└────────────────*
+
+*┌─MELHORAR O GRUPO─*
+*│-.antiracismo [on/off]*
+*│-.antilink [1/0]*
+*└────────────────* 
+
+*┌─COMANDOS SEM PREFIX🤖─* 
+*│-bah*
+*│-oii*
+*│-bv*
+*│-canta ai bot*
+*│-grita*
+*│-gemidao*
+*│-musica*
+*└────────────────* 
+*Só os ADMS que pode usa esse comando meu lindo.*`
 }
 exports.help1 = help1
 
